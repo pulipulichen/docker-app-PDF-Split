c6f0884a59e793e0860042607f1c2b72c23803c5
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-01-03 00:30:09 +0800

    Tue Jan  3 12:30:09 AM CST 2023

--- app/lib-pdf/ExtractSplitInformation.js
+++ app/lib-pdf/ExtractSplitInformation.js
@@ -30,13 +30,14 @@ module.exports = async function (inputFile) {
 			}
 
       let sub_filename = contents.slice(1).trim()
-      sub_filename = sub_filename.replace(/[\u{0080}-\u{FFFF}]/gu, " ")
+      // sub_filename = sub_filename.replace(/[\u{0080}-\u{FFFF}]/gu, " ")
       sub_filename = sub_filename.replace(/\n/g, ' ')
       sub_filename = sub_filename.replace(/\r/g, ' ')
       sub_filename = sub_filename.replace(/\t/g, ' ')
       while (sub_filename.indexOf('  ') > -1) {
-        sub_filename = sub_filename.replace(/  /g, ' ')
+        sub_filename = sub_filename.replace(/  /g, ' ').trim()
       }
+      sub_filename = sub_filename.trim()
 			// console.log(page_number, sub_filename)
 
       splitInformation.push({
